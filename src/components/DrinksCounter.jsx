576861af4df7d11645f4bdf422ae704fd1ae7fdb
RuslanGon <ruslan441006@gmail.com>
2025-11-13 12:34:19 +0200

    wer

--- src/components/DrinksCounter.jsx
+++ src/components/DrinksCounter.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DrinksValues from './DrinksValues.jsx'
 
 const DrinksCounter = () => {
+
+const initialDrinks = {beer:3, wisky:5, vine:1}     
 
 const handleLogDrink = (drinkName) => {
     console.log(drinkName);
 }    
 
+const [counter, setCounter] = useState(0)
+
+const increment = () => {
+setCounter(counter + 1)
+
+}
+
+const decrement = () => {
+    setCounter(counter - 1)
+    }
+
+
   return (
     <div>
-         <DrinksValues drinks={{beer:3, wisky:5, vine:1 }} />
+        <button onClick={increment}>counter: {counter}</button>
+        <button onClick={decrement}>-</button>
+
+         <DrinksValues drinks={initialDrinks} />
         <button onClick={() => handleLogDrink('Beer')}>Beer 🍺</button>
         <button onClick={() => handleLogDrink('Wisky')}>Wisky 🥃</button>
         <button onClick={() => handleLogDrink('Vine')}>Vine 🍷</button>
