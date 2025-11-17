e1d762fd1c54e4622466a9fbae615fdb83b317c9
RuslanGon <ruslan441006@gmail.com>
2025-11-17 13:29:43 +0200

    wer

--- src/components/MailBoxForm.jsx
+++ src/components/MailBoxForm.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const MailBoxForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const userEmail = e.currentTarget.elements.userEmail.value;
+        const userName = e.currentTarget.elements.userName.value
+        console.log(userEmail, userName);
+      };
+    
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <h2>Add new user</h2>
         <label>
             <span>user email:</span>
             <br />
-            <input type="email" name='userEmail' placeholder='your@name' required  />
+            <input type="email" name='userEmail' placeholder='your@email' required  />
+        </label>
+        <br />
+        <label>
+            <span>user name:</span>
+            <br />
+            <input type="text" name='userName' placeholder='your@name' required  />
         </label>
         <br />
         <button type='submit'>Create new user</button>
