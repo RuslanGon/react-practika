708adbaa496b2c0a17db4ff5fd6dcfd80ad49de5
RuslanGon <ruslan441006@gmail.com>
2025-11-18 12:11:52 +0200

    wre

--- src/components/MailBoxForm.jsx
+++ src/components/MailBoxForm.jsx
@@ -1,25 +1,31 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
 import * as yup from "yup";
 
 const initialValues = {
   userName: "",
   userEmail: "",
-  favColor: ""
+  favColor: "",
 };
 
 const mailBoxSchema = yup.object().shape({
-  userName: yup.string().min(2, "Too Short!").required("enter your name, please"),
+  userName: yup
+    .string()
+    .min(2, "Too Short!")
+    .required("enter your name, please"),
 
-  userEmail: yup.string().email("Must be a valid email!")
+  userEmail: yup
+    .string()
+    .email("Must be a valid email!")
     .required("enter your email, please"),
 
-  favColor: yup.string().oneOf(['red', 'green', 'blue'], "enter your color, please")
-    .required("enter your color, please")
+  favColor: yup
+    .string()
+    .oneOf(["red", "green", "blue"], "enter your color, please")
+    .required("enter your color, please"),
 });
 
 const MailBoxForm = ({ onAddUser }) => {
-
   const handleSubmit = (values, actions) => {
     onAddUser(values);
     actions.resetForm();
@@ -44,22 +50,22 @@ const MailBoxForm = ({ onAddUser }) => {
         </label>
         <br />
         <div>
-  <span>favourite color:</span>
-  <br />
-  <label>
-    <Field type="radio" name="favColor" value="red" />
-    Red
-  </label>
-  <label>
-    <Field type="radio" name="favColor" value="green" />
-    Green
-  </label>
-  <label>
-    <Field type="radio" name="favColor" value="blue" />
-    Blue
-  </label>
-  <ErrorMessage name="favColor" component="span" />
-</div>
+          <span>favourite color:</span>
+          <br />
+          <label>
+            <Field type="radio" name="favColor" value="red" />
+            Red
+          </label>
+          <label>
+            <Field type="radio" name="favColor" value="green" />
+            Green
+          </label>
+          <label>
+            <Field type="radio" name="favColor" value="blue" />
+            Blue
+          </label>
+          <ErrorMessage name="favColor" component="span" />
+        </div>
         <br />
         <button type="submit">Create new user</button>
       </Form>
