d8c968c1bd3ff94227cfc005029270ec2e35bcf9
RuslanGon <ruslan441006@gmail.com>
2025-11-18 10:50:51 +0200

    wer

--- src/components/MailBoxForm.jsx
+++ src/components/MailBoxForm.jsx
@@ -1,31 +1,39 @@
-import { Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import React from 'react'
+import * as yup from "yup";
+
+const initialValues = {
+  userName: "",
+  userEmail: ""
+};
+
+const userSchema = yup.object().shape({
+  userName: yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+  userEmail: yup.string().email("Must be a valid email!").required("Required"),
+});
 
 const MailBoxForm = ({onAddUser}) => {
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const userEmail = e.currentTarget.elements.userEmail.value;
-      const userName = e.currentTarget.elements.userName.value;
-      const formData = {userEmail, userName};
-      onAddUser(formData)
-      e.currentTarget.reset();
+     const handleSubmit = (values, actions) => {
+      // onAddUser(formData)
+      console.log(values);
+      actions.resetForm();
     };
     
   return (
-    <Formik initialValues={{}} onSubmit={handleSubmit}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchem={userSchema}>
     <Form>
         <h2>Add new user</h2>
         <label>
             <span>user email:</span>
             <br />
-            <input type="email" name='userEmail' placeholder='your@email' required  />
+            <Field type="email" name="userEmail" />
         </label>
         <br />
         <label>
             <span>user name:</span>
             <br />
-            <input type="text" name='userName' placeholder='name' required  />
+            <Field type="text" name="userName" />
         </label>
         <br />
         <button type='submit'>Create new user</button>
