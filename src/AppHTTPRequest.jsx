cc1e7ff47f136417a6d5a591e14eb3a51e886a7b
RuslanGon <ruslan441006@gmail.com>
2025-11-19 11:41:11 +0200

    wer

--- src/AppHTTPRequest.jsx
+++ src/AppHTTPRequest.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react'
 
 const AppHTTPRequest = () => {
 
-const [products, setProdukts] = useState(null)
+const [products, setProducts] = useState(null)
 
 useEffect(() => {
 async function fetchProducts () {
   const {data} = await axios.get('https://dummyjson.com/products')
-  console.log(data);
-  return response
+  // console.log(data);
+setProducts(data.products)
 }
 fetchProducts()
 },[])
@@ -18,13 +18,15 @@ fetchProducts()
     <div>
       <h1>Products smart marker</h1>
       <ul>
-        <li>
-          <img src="" alt="img" />
-          <h2>Title:</h2>
-          <p>Description:</p>
-          <h3>Price:</h3>
-          <p>Rating:</p>
-        </li>
+      {Array.isArray(products) && products.map(product => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} width={150} />
+            <h2>Title: {product.title}</h2>
+            <p>Description: {product.description}</p>
+            <h3>Price: {product.price}$</h3>
+            <p>Rating: {product.rating}</p>
+          </li>
+        ))}
       </ul>
     </div>
   )
