90961f070517a7c727ead9088f06be939e6b18d1
RuslanGon <ruslan441006@gmail.com>
2025-11-28 14:12:28 +0200

    wrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import css from "./App.module.css";
-import { Routes, Route, NavLink } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import MailBoxPage from "./pages/MailBoxPage.jsx";
-import SearchPage from "./pages/SearchPage.jsx";
-import ProductsPage from "./pages/ProductsPage.jsx";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+const getActiveClass = ({ isActive }) =>
+  clsx(css.navLink, { [css.active]: isActive });
 
 const App = () => {
+
   return (
     <div>
       <header>
       <nav className={css.nav}>
-      <NavLink className={css.navLink} to="/mailbox">MailBox</NavLink>
-        <NavLink className={css.navLink} to="/products">Products</NavLink>
-        <NavLink className={css.navLink} to="/search">Search Page</NavLink>
+        <NavLink className={getActiveClass} to="/mailbox">MailBox</NavLink>
+        <NavLink className={getActiveClass} to="/products">Products</NavLink>
+        <NavLink className={getActiveClass} to="/search">Search Page</NavLink>
       </nav>
       </header>
     </div>
