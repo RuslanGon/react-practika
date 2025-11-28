6b5b59a5b11fe2643230e691db94c46b26fad847
RuslanGon <ruslan441006@gmail.com>
2025-11-28 16:21:14 +0200

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../components/Loader.jsx";
+import Error from "../components/Error.jsx";
+import { requestProductDetailsById } from "../services/api.js";
+import css from "./ProductDetailsPage.module.css";
 
 const ProductDetailsPage = () => {
+  const { productId } = useParams();
+  const [productDetails, setProductDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-const {productId} = useParams()    
+  useEffect(() => {
+    async function fetchProductDetails() {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const data = await requestProductDetailsById(productId);
+        setProductDetails(data);
+      } catch (error) {
+        console.error(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (productId) fetchProductDetails();
+  }, [productId]);
+
+  if (isLoading) return <Loader />;
+  if (isError) return <Error />;
 
   return (
-    <div>
-        <h1>Product details: {productId}</h1>
-        <div>
-            <img src="" alt="img" />
-            <h2>Title:</h2>
-            <p>Brand:</p>
-            <h3>Price:</h3>
+    <div className={css.container}>
+      {productDetails && (
+        <div className={css.card}>
+          <img
+            className={css.image}
+            src={productDetails.thumbnail}
+            alt={productDetails.title}
+          />
+          <div className={css.info}>
+            <h2 className={css.title}>{productDetails.title}</h2>
+            <p className={css.brand}>Brand: {productDetails.brand}</p>
+            <h3 className={css.price}>Price: ${productDetails.price}</h3>
+          </div>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
--- src/services/api.js
+++ src/services/api.js
@@ -13,3 +13,8 @@ export const requestProductsByQuery = async (query = '') => {
     const { data } = await instance.get(`/products/search?q=${query}`)
     return data
   }
+
+  export const requestProductDetailsById = async (productId) => {
+    const { data } = await instance.get(`/products/${productId}`)
+    return data
+  }  
