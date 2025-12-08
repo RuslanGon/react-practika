ececb58ed5bc2f2b635d58150c8b270650d20883
RuslanGon <ruslan441006@gmail.com>
2025-12-08 16:20:23 +0200

    wer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,46 @@
-import { mailboxReducer } from "./mailbox/mailboxReducer.js";
 import { configureStore } from "@reduxjs/toolkit";
+import { mailboxReducer } from "./mailbox/mailboxReducer.js";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+
+const mailboxPersistConfig = {
+  key: "mailbox",
+  storage,
+  whitelist: ["users", "filter"],
+};
+
+const persistedMailboxReducer = persistReducer(mailboxPersistConfig, mailboxReducer);
 
 export const store = configureStore({
   reducer: {
-    mailbox: mailboxReducer
-  }
-})
+    mailbox: persistedMailboxReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+// -------------------------- 
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { mailboxReducer } from "./mailbox/mailboxReducer.js";
+
+// export const store = configureStore({
+//   reducer: {
+//     mailbox: mailboxReducer
+//   }
+// });
