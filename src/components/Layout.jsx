0fde08c753776c3b47bc8c3043964ae3e0c46f98
RuslanGon <ruslan441006@gmail.com>
2025-12-12 13:24:18 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,36 +1,57 @@
-import clsx from 'clsx';
-import React from 'react'
-import css from '../App.module.css'
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectAuthIsSignedIn } from '../redux/auth/selectors.js';
+import clsx from "clsx";
+import React from "react";
+import css from "../App.module.css";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectAuthIsSignedIn } from "../redux/auth/selectors.js";
 
 const getActiveClass = ({ isActive }) =>
-    clsx(css.navLink, { [css.active]: isActive });
+  clsx(css.navLink, { [css.active]: isActive });
 
-const Layout = ({children}) => {
-
-  const isSignetIn = useSelector(selectAuthIsSignedIn)   
-
+const Layout = ({ children }) => {
+  const isSignetIn = useSelector(selectAuthIsSignedIn);
 
   return (
     <div>
-          <header>
-      <nav className={css.nav}>
-      <NavLink className={getActiveClass} to="/">Home Page</NavLink>
-      {isSignetIn ? <></> : <></>}
-        <NavLink className={getActiveClass} to="/mailbox">MailBox</NavLink>
-        <NavLink className={getActiveClass} to="/products">Products</NavLink>
-        <NavLink className={getActiveClass} to="/search">Search Page</NavLink>
-        <NavLink className={getActiveClass} to="/drinks">Drinks</NavLink>
-        <NavLink className={getActiveClass} to="/contacts">ContactsPage</NavLink>
-        <NavLink className={getActiveClass} to="/login">Login</NavLink>
-        <NavLink className={getActiveClass} to="/register">Register</NavLink>
-      </nav>
+      <header>
+        <nav className={css.nav}>
+          <NavLink className={getActiveClass} to="/">
+            Home Page
+          </NavLink>
+          {isSignetIn ? (
+            <>
+              <NavLink className={getActiveClass} to="/mailbox">
+                MailBox
+              </NavLink>
+              <NavLink className={getActiveClass} to="/products">
+                Products
+              </NavLink>
+              <NavLink className={getActiveClass} to="/search">
+                Search Page
+              </NavLink>
+              <NavLink className={getActiveClass} to="/drinks">
+                Drinks
+              </NavLink>
+              <NavLink className={getActiveClass} to="/contacts">
+                ContactsPage
+              </NavLink>
+            </>
+          ) : (
+            <>
+              {" "}
+              <NavLink className={getActiveClass} to="/login">
+                Login
+              </NavLink>
+              <NavLink className={getActiveClass} to="/register">
+                Register
+              </NavLink>
+            </>
+          )}
+        </nav>
       </header>
       <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
